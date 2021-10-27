2e9573aa4426abb6df621dbd465169e1bf1e4d4e
Stephen Davis <stephen.davis2016@gmail.com>
2021-10-27 17:13:21 +0100

    Upgrading data structure represeting graph to represent DAG

--- LowestCommonAncestor1/src/LowestCommonAncestor1.java
+++ LowestCommonAncestor1/src/LowestCommonAncestor1.java
@@ -3,22 +3,22 @@ public class LowestCommonAncestor1 {
 	/* Driver program to test LCA functions */
 	public static void main(String args[])
 	{
-		DAG tree = new DAG();
-		tree.root = new Node(1);
-		tree.root.leftChild = new Node(2);
-		tree.root.rightChild = new Node(3);
-		tree.root.leftChild.leftChild = new Node(4);
-		tree.root.leftChild.rightChild = new Node(5);
-		tree.root.rightChild.leftChild = new Node(6);
-		tree.root.rightChild.rightChild = new Node(7);
+		DAG graph = new DAG();
+		graph.root = new Node(1);
+		graph.root.leftChild = new Node(2);
+		graph.root.rightChild = new Node(3);
+		graph.root.leftChild.leftChild = new Node(4);
+		graph.root.leftChild.rightChild = new Node(5);
+		graph.root.rightChild.leftChild = new Node(6);
+		graph.root.rightChild.rightChild = new Node(7);
 
-		Node lca = tree.findLCA(4, 5);
+		Node lca = graph.findLCA(4, 5);
 		if (lca != null)
 			System.out.println("LCA(4, 5) = " + lca.data);
 		else
 			System.out.println("Keys are not present");
 
-		lca = tree.findLCA(4, 10);
+		lca = graph.findLCA(4, 10);
 		if (lca != null)
 			System.out.println("LCA(4, 10) = " + lca.data);
 		else
